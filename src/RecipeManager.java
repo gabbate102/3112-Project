64602590e32109d87fc97483f3caf6b88ee54402
Giovanni A <32625900+gabbate102@users.noreply.github.com>
2024-03-16 15:57:07 -0400

    Refactor RecipeManager to use ObjectOutputStream for writing recipes

--- src/RecipeManager.java
+++ src/RecipeManager.java
@@ -1,6 +1,8 @@
 package src;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.ObjectOutputStream;
 import java.io.File;
 
 public class RecipeManager {
@@ -29,7 +31,7 @@ public class RecipeManager {
    * readRecipes method reads the recipes from recipeListFile
    */
   private void readRecipes() {
-    
+
   }
   /**
    * writeRecipe method writes a recipe to the recipeListFile
@@ -37,9 +39,11 @@ public class RecipeManager {
   private void writeRecipe(Recipe recipe) {
     try {
       // work in progress.
-      FileWriter writer = new FileWriter(recipeListFile);
-      writer.write(recipe);
-      writer.close();
+      FileOutputStream fileOut = new FileOutputStream(recipeListFile);
+      ObjectOutputStream out = new ObjectOutputStream(fileOut);
+      out.writeObject(recipe);
+      out.close();
+      fileOut.close();
     } catch (IOException e) {
       System.out.println(e);
     }
