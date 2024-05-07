9ca3fb61afa7d2ba015385e49143406a7ed2bbd1
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-05-07 14:26:16 -0400

    Made recipeParse.java more readable

--- src/RecipeParse.java
+++ src/RecipeParse.java
@@ -21,38 +21,53 @@ public class RecipeParse {
    * @throws IOException 
    */
   public static Recipe parseRecipe(String url) throws IOException {
+    
     Recipe recipe = new Recipe();
     recipe.setURL(url);
+
     // downloading the target website with an HTTP GET request
     Document doc = Jsoup.connect(url).get();
     
     // WordPress Recipe Maker parsing
     // Get the recipe block
     Elements recipeBlock = doc.getElementsByClass("wprm-recipe");
+
     // Check if recipeBlock is empty, return an empty recipe if it is
     if (recipeBlock.toString().length() < 1) {
       return recipe;
     } else {
+
       // get recipeName
       Elements recipeName = doc.getElementsByClass("wprm-recipe-name");
       System.out.println(recipeName.text());
       recipe.setRecipeName(recipeName.text());
+
       // get recipeIngredients
-      ArrayList<String> ingredientsArrayList = new ArrayList();
+      ArrayList<String> ingredientsArrayList = new ArrayList<String>();
       Elements siteIngredientsSection = doc.getElementsByClass("wprm-recipe-ingredient-group");
+
+      // for each section of the ingredients list
       for (Element item : siteIngredientsSection) {
+
+        // get the section name
         Elements siteSectionName = item.getElementsByClass("wprm-recipe-group-name");
         ingredientsArrayList.add("Section: " + siteSectionName);
+
+        // get the ingredient list
+
         Elements siteIngredientList = item.getElementsByTag("ul");
         // iterate over the ingredient list
         for (Element ingredient : siteIngredientList) {
           System.out.println(ingredient.text());
+
+          // add the ingredient to ingredientsArrayList
           ingredientsArrayList.add(ingredient.text());
         }
       }
       // convert the arraylist to an array
       String[] ingredientsList = new String[ingredientsArrayList.size()];
       ingredientsList = ingredientsArrayList.toArray(ingredientsList);
+
       // set the ingredients property of the recipe
       recipe.setIngredients(ingredientsList);
     }
