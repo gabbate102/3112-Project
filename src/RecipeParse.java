d7ff2c9ef9f23176914bfd53b1ecc2d7db89f370
Giovanni A. <32625900+gabbate102@users.noreply.github.com>
2024-05-08 14:56:25 -0400

    Refactored RecipeParse to be more flexible in the future

--- src/RecipeParse.java
+++ src/RecipeParse.java
@@ -28,78 +28,90 @@ public class RecipeParse {
     // downloading the target website with an HTTP GET request
     Document doc = Jsoup.connect(url).get();
     
-    // WordPress Recipe Maker parsing
+    // Check for WordPress Recipe Maker parsing
     // Get the recipe block
     Elements recipeBlock = doc.getElementsByClass("wprm-recipe");
 
     // Check if recipeBlock is empty, return an empty recipe if it is
-    if (recipeBlock.toString().length() < 1) {
-      return recipe;
+    if (recipeBlock.toString().length() > 1) {
+      // call wprmParse
+      recipe = wprmParse(doc, recipe);
     } else {
-
-      // get recipeName
-      Elements recipeName = doc.getElementsByClass("wprm-recipe-name");
-      System.out.println(recipeName.text());
-      recipe.setRecipeName(recipeName.text());
-
-      // Get the Ingredients  ----------
-      // get the ingredients section of the site
-      ArrayList<String> ingredientsArrayList = new ArrayList<String>();
-      Elements siteIngredientsSection = doc.getElementsByClass("wprm-recipe-ingredient-group");
-
-      // for each section of the ingredients list
-      for (Element item : siteIngredientsSection) {
-
-        // get the section name
-        Elements siteSectionName = item.getElementsByClass("wprm-recipe-group-name");
-        ingredientsArrayList.add("Section: " + siteSectionName.text());
-
-        // get the ingredient list
-
-        Elements siteIngredientList = item.getElementsByTag("ul");
-        // iterate over the ingredient list
-        for (Element ingredient : siteIngredientList) {
-          String currentIngredient = ingredient.text().replace(",", " ") + ",";
-          System.out.println(currentIngredient);
-
-          // add the ingredient to ingredientsArrayList
-          ingredientsArrayList.add(currentIngredient);
-        }
-      }
-
-      // convert the arraylist to an array
-      String[] ingredientsList = new String[ingredientsArrayList.size()];
-      ingredientsList = ingredientsArrayList.toArray(ingredientsList);
-
-      // set the ingredients property of the recipe
-      recipe.setIngredients(ingredientsList);
-
-      // Get the Procedure ----------
-
-      ArrayList<String> procedureArrayList = new ArrayList<String>();
-
-      // get the siteProcedureSections
-      Elements siteProcedureSections = doc.getElementsByClass("wprm-recipe-instructions");
-
-      // for each section, get the list items
-      for (Element section : siteProcedureSections) {
-
-        Elements listItems = section.getElementsByTag("li");
-
-        // for each list item, add the text to procedureArrayList
-        for (Element li : listItems) {
-          procedureArrayList.add(li.text());
-        }
-      }
-
-      // convert the arraylist to an array
-      String[] procedureList = new String[procedureArrayList.size()];
-      procedureList = procedureArrayList.toArray(procedureList);
-
-      // set the procedure property of the recipe
-      recipe.setProcedure(procedureList);
+      return recipe;
     }
     
+    return recipe;
+  }
+
+  /**
+   * Parses websites that use Wordpress Recipe Maker
+   * @param doc
+   * @param recipe
+   * @return Recipe
+   */
+  private static Recipe wprmParse(Document doc, Recipe recipe) {
+    // get recipeName
+    Elements recipeName = doc.getElementsByClass("wprm-recipe-name");
+    System.out.println(recipeName.text());
+    recipe.setRecipeName(recipeName.text());
+
+    // Get the Ingredients  ----------
+    // get the ingredients section of the site
+    ArrayList<String> ingredientsArrayList = new ArrayList<String>();
+    Elements siteIngredientsSection = doc.getElementsByClass("wprm-recipe-ingredient-group");
+
+    // for each section of the ingredients list
+    for (Element item : siteIngredientsSection) {
+
+      // get the section name
+      Elements siteSectionName = item.getElementsByClass("wprm-recipe-group-name");
+      ingredientsArrayList.add("Section: " + siteSectionName.text());
+
+      // get the ingredient list
+
+      Elements siteIngredientList = item.getElementsByTag("ul");
+      // iterate over the ingredient list
+      for (Element ingredient : siteIngredientList) {
+        String currentIngredient = ingredient.text().replace(",", " ") + ",";
+        System.out.println(currentIngredient);
+
+        // add the ingredient to ingredientsArrayList
+        ingredientsArrayList.add(currentIngredient);
+      }
+    }
+
+    // convert the arraylist to an array
+    String[] ingredientsList = new String[ingredientsArrayList.size()];
+    ingredientsList = ingredientsArrayList.toArray(ingredientsList);
+
+    // set the ingredients property of the recipe
+    recipe.setIngredients(ingredientsList);
+
+    // Get the Procedure ----------
+
+    ArrayList<String> procedureArrayList = new ArrayList<String>();
+
+    // get the siteProcedureSections
+    Elements siteProcedureSections = doc.getElementsByClass("wprm-recipe-instructions");
+
+    // for each section, get the list items
+    for (Element section : siteProcedureSections) {
+
+      Elements listItems = section.getElementsByTag("li");
+
+      // for each list item, add the text to procedureArrayList
+      for (Element li : listItems) {
+        procedureArrayList.add(li.text());
+      }
+    }
+
+    // convert the arraylist to an array
+    String[] procedureList = new String[procedureArrayList.size()];
+    procedureList = procedureArrayList.toArray(procedureList);
+
+    // set the procedure property of the recipe
+    recipe.setProcedure(procedureList);
+
     return recipe;
   }
 }
